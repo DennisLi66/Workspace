b462dcde33570e02273a71c2dee21944f670d518
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-04-20 22:12:21 -0700

    Basic Home

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-require('dotenv').config(); //INSTALL THIS ONE
+require('dotenv').config();
 const express = require("express");
-const https = require("https");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
+const cookieParser = require('cookie-parser');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
+const randomatic = require('randomatic');
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+// var connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+//   multipleStatements: true
+// })
+// connection.connect(); //FIX THIS: write dotenv file
+//
+
 
 //homepage
 app.get("/",function(req,res){
+  //if user logged in redirect to dashboard
+  console.log("J");
+  res.render('home',{banner: 'Workspace: Welcome to Workspace!'})
+})
+
+app.get("/dashboard",function(req,res){
 
 })
+
+app.route("/register")
+  .get(function(req,res){
+    res.render("register",{banner: 'Workspace: Registration'})
+  })
+  .post()
+
+app.route("/login")
+  .get(function(res,res){
+    res.render("login",{banner:'Workspace: Login'})
+  })
+  .post()
 
 app.get("/about",function(req,res){
 
 })
 
-app.route("/register")
-  .get()
-  .post()
-
-app.route("/login")
-  .get()
-  .post()
 
 app.get("/profile/:userid",function(req,res){})
 
@@ -31,14 +60,16 @@ app.get("/messages",function(req,res){})
 
 app.get("/messages/:userid",function(req,res){})
 
-app.get("/dashboard",function(req,res))
+app.get("/calendar",function(req,res){})
 
-app.get("/calendar",function(req,res))
+app.get("/todo/",function(req,res){})
 
-app.get("/todo/",function(req,res))
+app.get("/todo/:year/:month/:day",function(req,res){})
 
-app.get("/todo/:year/:month/:day",function(req,res))
+app.get("/todo/:year/:month",function(req,res){})
 
-app.get("/todo/:year/:month",function(req,res))
+app.get("/todo/:year",function(req,res){})
 
-app.get("/todo/:year",function(req,res))
+app.listen(3000, function() {
+  console.log("Server Started.")
+});
